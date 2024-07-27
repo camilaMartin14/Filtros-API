a34e638f648c2563a55c86ca06287853073e04e2
camilaMartin14 <camilamartin1403@gmail.com>
2024-07-27 10:25:31 -0300

    subir

---
+++ src/test/java/com/filtrosapi/filtros/FiltrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filtrosapi.filtros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
